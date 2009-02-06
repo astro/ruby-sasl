423c2c4d55e51ce26d602324a718dfa82f892053
Astro <astro@spaceboyz.net>
2009-02-06 23:47:39 +0100

    lib/sasl.rb: do not load editor backups

--- lib/sasl.rb
+++ lib/sasl.rb
@@ -1,4 +1,4 @@
 SASL_PATH = File.dirname(__FILE__) + "/sasl"
 Dir.foreach(SASL_PATH) do |f|
-  require "#{SASL_PATH}/#{f}" if f =~ /\.rb$/
+  require "#{SASL_PATH}/#{f}" if f =~ /^[^\.].+\.rb$/
 end
